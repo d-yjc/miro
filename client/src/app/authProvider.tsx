a3696767c40e25094352562d35f1313c8ce2ed64
d-yjc <cho.yj.daniel@gmail.com>
2025-07-12 22:33:28 +0100

    Initialised Cognito authentication.

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -1,19 +1,54 @@
 import React from "react";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure({
-    Auth: {
-        Cognito: {
-            userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID,
-            userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID 
-        }
-    }
-})
+  Auth: {
+    Cognito: {
+      userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "",
+      userPoolClientId:
+        process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID || "",
+    },
+  },
+});
+
+const formFields = {
+  signUp: {
+    username: {
+      order: 1,
+      placeholder: "Enter a username",
+      label: "User",
+      isRequired: true,
+    },
+    email: {
+      order: 2,
+      placeholder: "Enter an email",
+      label: "E-mail",
+      isRequired: true,
+      type: "email",
+    },
+    password: {
+      order: 3,
+      placeholder: "Enter a strong password",
+      label: "Password",
+      isRequired: true,
+      type: "password",
+    },
+    confirm_password: {
+      order: 4,
+      placeholder: "Re-enter password",
+      label: "Confirm Password",
+      isRequired: true,
+      type: "password",
+    },
+  },
+};
+
 const AuthProvider = ({ children }: any) => {
   return (
     <div className="mt-5">
-      <Authenticator>
+      <Authenticator formFields={formFields}>
         {({ user }: any) =>
           user ? <div>{children}</div> : <div>Sign in below: </div>
         }
