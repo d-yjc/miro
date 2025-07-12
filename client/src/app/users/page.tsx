8b55ab3e7780b13446018ddfbec1d814a14a78b5
d-yjc <cho.yj.daniel@gmail.com>
2025-07-12 20:51:18 +0100

    Amended image sources for s3.

--- client/src/app/users/page.tsx
+++ client/src/app/users/page.tsx
@@ -27,7 +27,7 @@ const columns: GridColDef[] = [
       <div className="flex h-full w-full items-center justify-center">
         <div className="size-9">
           <Image
-            src={`/${params.value}`}
+            src={`https://miro-s3-images.s3.us-east-1.amazonaws.com/${params.value}`}
             alt={params.row.username}
             width={100}
             height={50}
